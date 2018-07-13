1affb5676b932a23d892c2b78e69fbcd7c948a94
Leandro Agugliari <leandro.agugliari@gmail.com>
2018-07-12 23:18:56 -0300

    Removendo interfaces uma vez que optei por usar o wiremock para os requestes de teste

--- src/main/java/ifood/component/OpenWeather.java
+++ src/main/java/ifood/component/OpenWeather.java
@@ -1,8 +1,63 @@
 package ifood.component;
 
+import ifood.component.OpenWeather;
+import ifood.exception.BaseException;
+import ifood.exception.ExceptionOriginEnum;
+import ifood.exception.InvalidCityException;
+import ifood.exception.InvalidOpenWeatherResponseException;
 import ifood.model.OpenWeatherResponse;
+import ifood.validator.CityValidator;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-public interface OpenWeather {
+import java.net.URI;
 
-    OpenWeatherResponse getCityTemp(final String cityname, final Double lat, final Double lon);
+@Component
+public class OpenWeather {
+
+    private final String appKey;
+
+    private final RestTemplate restTemplate;
+
+    private final String openWeatherBaseEndpoint;
+
+    @Autowired
+    public OpenWeather(@Value("${openWeather.endpoint}") final String openWeatherEndpoint,
+                       @Value("${openWeather.key}") final String appKey,
+                       final RestTemplate restTemplate) {
+        this.appKey = appKey;
+        this.restTemplate = restTemplate;
+        this.openWeatherBaseEndpoint = openWeatherEndpoint;
+    }
+
+    private URI getWeatherUri(final String cityname, final Double lat, final Double lon) {
+        CityValidator.validate(cityname, lat, lon);
+
+        final UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromUriString(openWeatherBaseEndpoint);
+        uriBuilder.queryParam("appid", appKey).queryParam("units", "metric");
+
+        return StringUtils.isBlank(cityname)
+                ? uriBuilder.queryParam("lat", lat).queryParam("lon", lon).build().toUri()
+                : uriBuilder.queryParam("q", cityname).build().toUri();
+    }
+
+    public OpenWeatherResponse getCityTemp(final String cityname, final Double lat, final Double lon) {
+        final URI uri = getWeatherUri(cityname, lat, lon);
+        try {
+            return restTemplate.getForObject(uri, OpenWeatherResponse.class);
+        } catch (HttpClientErrorException hcee) {
+            if (HttpStatus.NOT_FOUND.equals(hcee.getStatusCode())) {
+                throw new InvalidCityException(cityname, lat, lon, hcee);
+            }
+            throw new InvalidOpenWeatherResponseException(uri.toString(), hcee);
+        } catch (Exception ex) {
+            throw new BaseException(ex.getMessage(), ex, ExceptionOriginEnum.INTERNAL);
+        }
+    }
 }
--- src/main/java/ifood/component/Spotify.java
+++ src/main/java/ifood/component/Spotify.java
@@ -3,10 +3,94 @@ package ifood.component;
 import ifood.model.SpotifyPlaylistResponse;
 import ifood.model.SpotifyTracksResponse;
 import ifood.model.TrackCategoryEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-public interface Spotify {
+import java.net.URI;
 
-    SpotifyPlaylistResponse getPlaylist(final TrackCategoryEnum trackCategoryEnum, final String country, final String token);
+@Component
+@Slf4j
+public class Spotify {
 
-    SpotifyTracksResponse getTracks(final String playListId, final String token);
+    private final RestTemplate restTemplate;
+
+    private final String playlistBaseEndpoint;
+
+    private final String tracksBaseEndpoint;
+
+    @Autowired
+    public Spotify(@Value("${spotify.playlistEndpoint}") final String playlistBaseEndpoint,
+                   @Value("${spotify.tracksEndpoint}") final String tracksBaseEndpoint,
+                   final RestTemplate restTemplate) {
+        this.tracksBaseEndpoint = tracksBaseEndpoint;
+        this.playlistBaseEndpoint = playlistBaseEndpoint;
+        this.restTemplate = restTemplate;
+    }
+
+    private HttpEntity<String> createHttpHeader(final String token) {
+        final HttpHeaders header = new HttpHeaders();
+        header.set("Authorization", String.format("Bearer %s", token));
+        return new HttpEntity<>(header);
+    }
+
+    public SpotifyPlaylistResponse getPlaylist(final TrackCategoryEnum trackCategoryEnum,
+                                               final String country,
+                                               final String token) {
+        try {
+            final String playlistEndpoint = String.format(playlistBaseEndpoint, trackCategoryEnum.toString());
+            final UriComponentsBuilder playlistUriBuilder = UriComponentsBuilder.fromUriString(playlistEndpoint);
+
+            if (StringUtils.isNotBlank(country)) {
+                playlistUriBuilder.queryParam("country", country);
+            }
+
+            final URI uri = playlistUriBuilder.build().toUri();
+
+            log.info(uri.toString());
+            ResponseEntity<SpotifyPlaylistResponse> response =
+                    restTemplate.exchange(uri, HttpMethod.GET, createHttpHeader(token), SpotifyPlaylistResponse.class);
+
+            if (response.hasBody()) {
+                return response.getBody();
+            }
+
+        } catch (HttpClientErrorException hcee) {
+            if (hcee.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw hcee;
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return new SpotifyPlaylistResponse(null);
+    }
+
+    public SpotifyTracksResponse getTracks(final String playListId, final String token) {
+        try {
+            final String tracksEndpoint = String.format(tracksBaseEndpoint, playListId);
+            final UriComponentsBuilder trackUriBuilder = UriComponentsBuilder.fromUriString(tracksEndpoint);
+            final URI uri = trackUriBuilder.build().toUri();
+
+            log.info(uri.toString());
+            ResponseEntity<SpotifyTracksResponse> response =
+                    restTemplate.exchange(uri, HttpMethod.GET, createHttpHeader(token), SpotifyTracksResponse.class);
+
+            if (response.hasBody()) {
+                return response.getBody();
+            }
+        } catch (HttpClientErrorException hcee) {
+            if (hcee.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw hcee;
+            }
+        } catch (Exception ex) {
+            throw ex;
+        }
+        return new SpotifyTracksResponse(null);
+    }
 }
